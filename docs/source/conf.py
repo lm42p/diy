b33b722d200c0934d3231713848ff46371ea7cd7
lm42p <lovemachine42people@gmail.com>
2024-05-21 15:01:30 +0200

    rajout dans conf.py déplacement de import rtd them

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 
@@ -31,6 +31,8 @@ release = '1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_rtd_theme
+
 extensions = [
     'sphinx_rtd_theme',
 ]
