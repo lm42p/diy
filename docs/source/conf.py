e5c6a0cf99d46cf6f9b6fbb4b2c4c58cd6e1319b
lm42p <lovemachine42people@gmail.com>
2024-05-21 15:04:46 +0200

    rajout dans conf.py enlever import rtd them

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ release = '1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_rtd_theme
 
 extensions = [
     'sphinx_rtd_theme',
